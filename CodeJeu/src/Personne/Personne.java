45c49b225ca460dd70e4c8eda7f8e08d92f6d13f
BELLIN Clara <b17024442@L-024110A003-10.salsa.univ-amu.fr>
2018-12-19 15:45:13 +0100

    modifs Personne.java

--- CodeJeu/src/Personne/Personne.java
+++ CodeJeu/src/Personne/Personne.java
@@ -1,5 +1,17 @@
 package Personne;
 
-public class Personne {
-
+public abstract class Personne {
+	private int id;
+	private String nom;
+	
+	private int pos_x, pos_y;
+	private int visibilite;
+	
+	public Personne(int id, String nom, int x, int y, int visibilite) {
+		this.id = id;
+		this.nom = nom;
+		pos_x = x;
+		pos_y = y;
+		this.visibilite = visibilite;
+	}
 }
