c7eae27b05bde68bdbe412a867e7ac8c0e2dd15c
POUJOL Teddy <p17027265@L-024110A003-02.salsa.univ-amu.fr>
2019-01-18 14:05:06 +0100

    ajout d'un new panneau

---
+++ CodeJeu/src/Interface/ConsolePanneau.java
@@ -0,0 +1,46 @@
+package Interface;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+
+
+
+public class ConsolePanneau extends JPanel
+{
+	JTextArea textArea ;
+	public ConsolePanneau()
+	{
+		
+		super();
+		 textArea = new JTextArea(24, 80);
+		textArea.setBackground(Color.BLACK);
+		textArea.setForeground(Color.LIGHT_GRAY);
+		
+		 
+	}
+	
+	protected void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		Graphics2D g2d = (Graphics2D)g;
+		/*
+		 * System.setOut(new PrintStream(new OutputStream() {
+			@Override
+			public void write(int b) throws IOException {
+			textArea.append(String.valueOf((char) b));
+			 }
+			 }));
+			 */
+		g.drawRect(200, 200, 200, 500);
+		
+	}
+	
+}
